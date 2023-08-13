de171a86908627d631c10a58a0d47480bb9b144c
khldalrs <85517016+khldalrs@users.noreply.github.com>
2023-08-13 17:37:03 +0300

    Added the basic skeleton using Vite

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+
+
+function App() {
+    return (
+        <>
+            <Header />
+            
+            <div> 
+                <h1>React App</h1>
+            </div>
+        </>
+    );
+    
+}
+
+
+
+
+
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function header() {
+  return (
+    <header>
+        <div className="header">
+            <h1>Feedback UI</h1>
+        </div>
+    </header>
+  )
+}
+
+export default header
